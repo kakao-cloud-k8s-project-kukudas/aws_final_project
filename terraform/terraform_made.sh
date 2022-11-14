f7e8c38252b491a71b66776e3e87c472abb79b32
kyungwon3 <100257397+kyungwon3@users.noreply.github.com>
2022-11-14 17:30:26 +0900

    Update terraform_made.sh

--- terraform/terraform_made.sh
+++ terraform/terraform_made.sh
@@ -16,3 +16,25 @@ cluster_version  = \"1.20\" " > terraform.auto.tfvars
 terraform init
 terraform workspace new $name
 terraform plan
+
+#클러스터 초기 설정
+aws eks --region ap-northeast-1 update-kubeconfig --name $name
+
+#CA(cluster-autocaler) 설치
+echo "
+autoDiscovery:
+  clusterName: ${name} # eks cluster name
+
+awsRegion: ap-northeast-1 # eks region
+
+extraArgs:
+  logtostderr: true
+  stderrthreshold: info
+  v: 4
+  expander: random
+  scale-down-enabled: true " > values.yaml
+helm repo add autoscaler <https://kubernetes.github.io/autoscaler>
+
+helm repo update
+
+helm install cluster-autoscaler autoscaler/cluster-autoscaler --values=./valuse.yaml
