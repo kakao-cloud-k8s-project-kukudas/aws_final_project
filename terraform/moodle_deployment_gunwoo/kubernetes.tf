3261133dc390f0918eb249f2216e4b27101cf8d4
Administrator <admin@example.com>
2022-11-25 11:11:12 +0900

    initial commit

---
+++ terraform/moodle_deployment_gunwoo/kubernetes.tf
@@ -0,0 +1,145 @@
+//CREATE DEPLOYMENT - provide rds environment variables
+resource "kubernetes_deployment" "moodle" {
+  depends_on = [aws_db_instance.rds]
+  metadata {
+    name = "moodle"
+    labels = {
+      App = "moodle"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "moodle"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "moodle"
+        }
+      }
+      spec {
+        container {
+          image = "bitnami/moodle:latest"
+          name  = "moodle"
+          command = ["sh", "-c", "sleep 60 && /opt/bitnami/scripts/moodle/run.sh"]
+          env{
+            name = "MOODLE_DATABASE_HOST"
+            value = "${aws_db_instance.rds.address}"
+          }
+          env{
+            name = "MOODLE_DATABASE_PORT"
+            value = 3306
+          }
+          env{
+            name = "MOODLE_DATABASE_USER"
+            value = "${aws_db_instance.rds.username}"
+          }
+          env{
+            name = "MOODLE_DATABASE_PASSWORD"
+             value = "${aws_db_instance.rds.password}"
+          }
+          env{
+            name = "MOODLE_DATABASE_NAME"
+            value = "${aws_db_instance.rds.db_name}"
+          }
+          env{
+            name = "PHP_MAX_INPUT_VARS"
+            value = "8000"
+          }
+          env{
+            name = "MOODLE_SSLPROXY"
+            value = "true"
+          }
+          port {
+            container_port = 8080
+            name = "tcp"
+          }
+          port {
+            container_port = 8443
+            name = "ssll"
+          }
+          volume_mount {
+            name = "moodle-ps"
+            mount_path = "/opt/bitnami/apache2/htdocs/"
+          }
+          resources {
+            limits= {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests= {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      volume {
+        name = "moodle-ps"
+        persistent_volume_claim {
+          claim_name = "moodle-pv-claim"
+        }
+      }
+      }
+    }
+
+  }
+}
+
+
+//EXPOSE DEPLOYMENT - This creates a LoadBalancer, which routes traffic from //the external load balancer to pods with the matching selector.
+
+resource "kubernetes_service" "moodle" {
+  depends_on = [kubernetes_deployment.moodle]
+  metadata {
+    name = "moodle"
+  }
+  spec {
+    selector = {
+      App = "moodle"
+    }
+    port {
+      port        = 80
+      target_port = 8080
+      name = "tcp"
+    }
+    port {
+      port        = 443
+      target_port = 8443
+      name = "ssll"
+    }
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_storage_class" "kubeSC" {
+  metadata {
+    name = "kubese"
+  }
+  storage_provisioner = "kubernetes.io/aws-ebs"
+  reclaim_policy = "Retain"
+  parameters = {
+    type = "gp2"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "pvc" {
+  depends_on=[kubernetes_storage_class.kubeSC]
+  metadata {
+    name = "moodle-pv-claim"
+    labels = {
+      "app" = "moodle"
+    }
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+  }
+}
---
+++ terraform/moodle_deployment_gunwoo/security_group.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "secrgrp_moodle" {
+  name        = "Moodle_security_group"
+  description = "Allow SSH and HTTP"
+  vpc_id=module.vpc.vpc_id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "db from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
