44dfaed682fc4ed4024d3ba0ecc511c673e9a347
Administrator <admin@example.com>
2022-11-30 15:48:33 +0900

    add

--- terraform_script/original_terraform/helm.release.tf
+++ terraform_script/original_terraform/helm.release.tf
@@ -4,7 +4,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
   }
