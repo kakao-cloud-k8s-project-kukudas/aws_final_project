3f75a216bf74adc407547417831328e33a0d881e
Yang sujin <66259643+musuys@users.noreply.github.com>
2022-12-01 13:48:01 +0900

    Update security_group.tf

--- terraform/moodle_deployment_kyung/security_group.tf
+++ terraform/moodle_deployment_kyung/security_group.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "secrgrp_moodle" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "efs from VPC"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
